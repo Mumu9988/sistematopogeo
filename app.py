ca7272b52c756989f7a70da403588bd7bfc408ef
Mumu9988 <murillofontoura22@gmail.com>
2025-05-17 09:32:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,8 @@ def cadastrar():
     telefone = request.form['telefone']
     propriedade = request.form['propriedade']
     return f"Produtor {nome} cadastrado com sucesso!"
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
